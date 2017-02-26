7998e1006d85f708b36732333a2d01583f5c5c24
notbakaneko <notbakaneko@users.noreply.github.com>
2017-02-26 17:30:37 +0900

    support serving pre-existing file

--- lib/thumbnailer/file_origin.rb
+++ lib/thumbnailer/file_origin.rb
@@ -11,17 +11,23 @@ module Thumbnailer
     def call
       super
       raise ArgumentError, 'signature must be longer than 2 characters' unless signature && signature.length > 1
-      Profiler.profile('Thumbnailer::FileOutput') do
-        FileUtils.mkdir_p File.dirname(full_path)
-        # signature ||= file_digest(in_filename) if Application.config.auto_signature?
-        thumbnailer = Thumbnails::FromFile.new(input: in_filename, signature: signature)
-        # thumbnailer.find_or_create!(force: true, **opts)
-        thumbnailer.create!(size: opts[:size], quality: opts[:q], target: :file, filename: full_path.to_s)
+      if !use_cached? || !File.file?(full_path)
+        Profiler.profile('Thumbnailer::FileOutput') do
+          FileUtils.mkdir_p File.dirname(full_path)
+          # signature ||= file_digest(in_filename) if Application.config.auto_signature?
+          thumbnailer = Thumbnails::FromFile.new(input: in_filename, signature: signature)
+          # thumbnailer.find_or_create!(force: true, **opts)
+          thumbnailer.create!(size: opts[:size], quality: opts[:q], target: :file, filename: full_path.to_s)
+        end
       end
       serve_file(full_path.to_s)
     end
 
     private
+
+    def use_cached?
+      true
+    end
 
     def filename
       "#{signature[0..1]}/#{signature}.jpg"
