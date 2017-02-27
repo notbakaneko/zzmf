2d1681c38bf572198ce8f27be03941f7c37df0f6
notbakaneko <notbakaneko@users.noreply.github.com>
2017-02-27 20:26:27 +0900

    extract params validator; fix return 404 on not found

--- config.ru
+++ config.ru
@@ -3,10 +3,13 @@
 require_relative 'config/application'
 require_relative 'lib/thumbnailer/file_origin'
 # require_relative 'lib/thumbnailer/remote_origin'
+require_relative 'lib/request_validators/basic'
 
 require_relative 'lib/thumbnailer/file_output'
-Thumbnailer::FileOrigin::Action.send(:prepend, Thumbnailer::FileOutput)
+m = Thumbnailer::FileOrigin::Action.send(:prepend, Thumbnailer::FileOutput)
 # require_relative 'lib/thumbnailer/buffer_output'
-# Thumbnailer::FileOrigin::Action.send(:prepend, Thumbnailer::BufferOutput)
+# m = Thumbnailer::FileOrigin::Action.send(:prepend, Thumbnailer::BufferOutput)
+
+m.send(:include, RequestValidators::Basic)
 
 run Thumbnailer::FileOrigin::Run.new
--- lib/thumbnailer/file_origin.rb
+++ lib/thumbnailer/file_origin.rb
@@ -19,6 +19,7 @@ module Thumbnailer
         log_str = "#{request.request_method} #{request.path} #{request.path_info} #{request.params}"
         $stdout.puts "Start #{log_str}"
         return [404, { 'Content-Type' => 'text/plain' }, ['404 Not Found']] unless File.file?(in_filename)
+        validate!(request)
       end
 
       def opts
@@ -79,25 +80,12 @@ module Thumbnailer
 
       def call(env)
         request = ::Rack::Request.new(env)
-        validate!(request)
         Action.new(request).call
       rescue StandardError => e
         $stderr.puts e
         $stderr.puts e.backtrace
         headers = { 'Content-Type' => 'text/plain' }
         [500, headers, ['500 Internal Server Error']]
-      end
-
-      private
-
-      def validate!(request)
-        # rubocop:disable Metrics/LineLength, Style/NumericPredicate
-        # arbitrary
-        raise ArgumentError, 'l must be > 0 and < 10000' unless request.params['l'] && request.params['l'].to_i > 0 && request.params['l'].to_i < 10_000
-        raise ArgumentError, 'q must be >=0 and <= 100' unless request.params['q'] && request.params['q'].to_i >= 0 && request.params['q'].to_i <= 100
-        raise ArgumentError, 's must be >=1 and <= 2' if request.params['s'] && request.params['s'].to_f < 1 && request.params['s'].to_f > 2
-        rause ArgumentError, 'A filename is required' unless request.path_info && !request.path_info.empty?
-        # rubocop:enable Metrics/LineLength, Style/NumericPredicate
       end
     end
   end
