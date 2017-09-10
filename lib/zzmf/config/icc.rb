aa937d11d8f6ffd35abdcc34bec0371d2f61538a
notbakaneko <notbakaneko@users.noreply.github.com>
2017-09-10 18:31:16 +0900

    read icc profile config file location from ENV

--- lib/zzmf/config/icc.rb
+++ lib/zzmf/config/icc.rb
@@ -6,7 +6,7 @@ module Zzmf
   module Config #:nodoc:
     module Icc
       def self.config_path
-        File.join('config', 'icc')
+        ENV['ZZMF_ICC_PROFILES'] || File.join('config', 'icc')
       end
 
       def self.profile_path(profile)
@@ -15,16 +15,18 @@ module Zzmf
       end
 
       def self.config
-        @config ||= reload_yaml
+        @config ||= reload_yaml || {}
       end
 
       def self.reload_yaml
         nodes = read_config
-        nodes.to_ruby
+        nodes&.to_ruby
       end
 
       def self.read_config
-        Psych.parse_file(File.absolute_path(File.join(Zzmf::Config::Icc.config_path, 'icc.yml')))
+        file = File.absolute_path(File.join(Zzmf::Config::Icc.config_path, 'icc.yml'))
+        return unless File.exist?(file)
+        Psych.parse_file(file)
       end
     end
   end
