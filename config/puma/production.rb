29637d7c8ca748b7b7a2b31bf2d9c4d7535ef23d
notbakaneko <notbakaneko@users.noreply.github.com>
2017-09-14 15:13:31 +0900

    all the workers

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -8,7 +8,7 @@ quiet
 require 'puma_worker_killer'
 
 PumaWorkerKiller.config do |config|
-  config.ram           = 800
+  config.ram           = 3072
   config.frequency     = 10
   config.percent_usage = 0.95
   config.rolling_restart_frequency = 12 * 3600
