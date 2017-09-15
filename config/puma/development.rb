ee31e67b680bac858a241377f5ff5953761f79e8
notbakaneko <notbakaneko@users.noreply.github.com>
2017-09-15 21:35:48 +0900

    puma config update

--- config/puma/development.rb
+++ config/puma/development.rb
@@ -6,8 +6,8 @@ threads 4, 4
 require 'puma_worker_killer'
 
 PumaWorkerKiller.config do |config|
-  config.ram           = 500
-  config.frequency     = 5
+  config.ram           = 1024
+  config.frequency     = 60
   config.percent_usage = 0.95
   config.rolling_restart_frequency = 12 * 3600
   config.reaper_status_logs = true
