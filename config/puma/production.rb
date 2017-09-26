f59fd41af49512684243a0dc40b0a2e6360ed9a6
notbakaneko <notbakaneko@users.noreply.github.com>
2017-09-27 02:17:56 +0900

    no repear log

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -12,7 +12,7 @@ PumaWorkerKiller.config do |config|
   config.frequency     = 30
   config.percent_usage = 0.95
   config.rolling_restart_frequency = 12 * 3600
-  config.reaper_status_logs = true
+  config.reaper_status_logs = false
 
   config.pre_term = ->(worker) { puts "Worker #{worker.inspect} being killed" }
 end
