dc0c7b087996d36e1dffe18906eea17c8bc7a27f
notbakaneko <notbakaneko@users.noreply.github.com>
2017-10-03 12:09:22 +0900

    less frequent reporting

--- config/puma/development.rb
+++ config/puma/development.rb
@@ -7,7 +7,7 @@ require 'puma_worker_killer'
 
 PumaWorkerKiller.config do |config|
   config.ram           = 500
-  config.frequency     = 5
+  config.frequency     = 60
   config.percent_usage = 0.95
   config.rolling_restart_frequency = 12 * 3600
   config.reaper_status_logs = true
