03217bc5d7d6392cad2447d5c9d61705a2e40ac1
notbakaneko <notbakaneko@users.noreply.github.com>
2017-10-09 01:21:45 +0900

    refactor common Action portions

--- lib/zzmf/thumbnailer/file_origin.rb
+++ lib/zzmf/thumbnailer/file_origin.rb
@@ -2,6 +2,7 @@
 
 require 'rack'
 require 'rack/server'
+require_relative './base_action'
 require_relative '../config/icc'
 require_relative '../magic_number'
 require_relative '../thumbnails/image'
@@ -10,13 +11,7 @@ require_relative '../profiler'
 module Zzmf
   module Thumbnailer
     module FileOrigin
-      class Action
-        attr_reader :request
-
-        def initialize(request)
-          @request = request
-        end
-
+      class Action < ::Zzmf::Thumbnailer::BaseAction
         def call
           log_str = "#{request.request_method} #{request.path} #{request.path_info} #{request.params}"
           $stdout.puts "Start #{log_str}"
@@ -24,68 +19,8 @@ module Zzmf
           validate!(request)
         end
 
-        def create_opts
-          @create_opts ||= begin
-                             hash = {}
-                             hash[:quality] = request.params['q'].to_i if request.params['q']
-                             if request.params['l']
-                               hash[:width] = hash[:height] = request.params['l'].to_i
-                             else
-                               hash[:width] = request.params['w'].to_i
-                               hash[:height] = request.params['h'].to_i
-                             end
-
-                             hash[:strip] = request.params['strip'] != '0'
-
-                             profile = request.params['profile']
-                             if profile && !profile.empty?
-                               hash[:profile] = File.absolute_path(Zzmf::Config::Icc.profile_path(profile))
-                             end
-
-                             hash
-                           end
-        end
-
-        def opts
-          @opts ||= begin
-                      hash = {}
-                      hash[:profile] = request.params['profile']
-                      hash[:scale] = request.params['s'].to_f if request.params['s']
-                      hash[:upscale] = request.params['u'] == '1'
-
-                      hash
-                    end
-        end
-
         def in_filename
           @in_filename ||= File.join(Application.config.root_path, request.path_info)
-        end
-
-        def serve_binary(buffer)
-          response = Rack::Response.new
-          mime_type = Zzmf::MagicNumber.from_string(buffer).mime_type
-          response.headers['Content-Type'] = mime_type
-          response.write(buffer)
-          response.finish
-        end
-
-        def serve_file(filename)
-          # FIXME: support SendFile
-          response = Rack::Response.new
-          body = File.binread(filename)
-          mime_type = Zzmf::MagicNumber.from_string(body).mime_type
-          response.headers['Content-Type'] = mime_type
-          response.write(body)
-          response.finish
-        end
-
-        def signature
-          # force jpg everything
-          "#{File.basename(request.path_info, '.*')}.jpg"
-        end
-
-        def use_cached?
-          !request.params['force'] || request.params['force'].empty?
         end
 
         def file_digest(filename)
--- lib/zzmf/thumbnailer/remote_origin.rb
+++ lib/zzmf/thumbnailer/remote_origin.rb
@@ -4,6 +4,7 @@ require 'rack'
 require 'rack/server'
 require 'net/http'
 require 'uri'
+require_relative './base_action'
 require_relative '../config/icc'
 require_relative '../magic_number'
 require_relative '../thumbnails/image'
@@ -12,13 +13,7 @@ require_relative '../profiler'
 module Zzmf
   module Thumbnailer
     class RemoteOrigin
-      class Action
-        attr_reader :request
-
-        def initialize(request)
-          @request = request
-        end
-
+      class Action < ::Zzmf::Thumbnailer::BaseAction
         def call
           log_str = "#{request.request_method} #{request.path} #{request.path_info} #{request.params}"
           $stdout.puts "Start #{log_str}"
@@ -36,68 +31,8 @@ module Zzmf
           res.body
         end
 
-        def create_opts
-          @create_opts ||= begin
-                             hash = {}
-                             hash[:quality] = request.params['q'].to_i if request.params['q']
-                             if request.params['l']
-                               hash[:width] = hash[:height] = request.params['l'].to_i
-                             else
-                               hash[:width] = request.params['w'].to_i
-                               hash[:height] = request.params['h'].to_i
-                             end
-
-                             hash[:strip] = request.params['strip'] != '0'
-
-                             profile = request.params['profile']
-                             if profile && !profile.empty?
-                               hash[:profile] = File.absolute_path(Zzmf::Config::Icc.profile_path(profile))
-                             end
-
-                             hash
-                           end
-        end
-
-        def opts
-          @opts ||= begin
-                      hash = {}
-                      hash[:profile] = request.params['profile']
-                      hash[:scale] = request.params['s'].to_f if request.params['s']
-                      hash[:upscale] = request.params['u'] == '1'
-
-                      hash
-                    end
-        end
-
         def origin_data
           @origin_data ||= fetch_origin(request.path_info)
-        end
-
-        def serve_binary(buffer)
-          response = Rack::Response.new
-          mime_type = Zzmf::MagicNumber.from_string(buffer).mime_type
-          response.headers['Content-Type'] = mime_type
-          response.write(buffer)
-          response.finish
-        end
-
-        def serve_file(filename)
-          # FIXME: support SendFile
-          response = Rack::Response.new
-          body = File.binread(filename)
-          mime_type = Zzmf::MagicNumber.from_string(body).mime_type
-          response.headers['Content-Type'] = mime_type
-          response.write(body)
-          response.finish
-        end
-
-        def signature
-          # force jpg everything
-          "#{File.basename(request.path_info, '.*')}.jpg"
-        end
-
-        def use_cached?
-          !request.params['force'] || request.params['force'].empty?
         end
       end
 
