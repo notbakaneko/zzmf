7b9aae626d8443c8ba09aa9cf5e1923e97ba6279
bakaneko <baka+github@nekonyan.net>
2017-11-24 15:40:28 +0900

    skip reaper_status_logs by default in development

--- config/puma/development.rb
+++ config/puma/development.rb
@@ -10,7 +10,7 @@ PumaWorkerKiller.config do |config|
   config.frequency     = 60
   config.percent_usage = 0.95
   config.rolling_restart_frequency = 12 * 3600
-  config.reaper_status_logs = true
+  # config.reaper_status_logs = true
 
   config.pre_term = ->(worker) { puts "Worker #{worker.inspect} being killed" }
 end
